689b365f3efb92a868aa453f7a37ff6956b73f7b
froakie <smithkaitlin94@gmail.com>
2017-05-20 23:22:56 -0400

    Added comments to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521001110) do
+ActiveRecord::Schema.define(version: 20170521015815) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id_id"], name: "index_comments_on_user_id_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
